eb094b7565342ca4a56d6d1366eb761b3aea8e8d
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-09-02 15:04:40 +0300

    Updates

---
+++ precache-manifest.08eb7f6939d65074c442097e684c39ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29165b191a7e3e9f2ab8a8bdf545667",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe55ffbb04a61df1adde",
+    "url": "./static/css/main.8047d52a.chunk.css"
+  },
+  {
+    "revision": "3716328de483ed560434",
+    "url": "./static/js/2.432399ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.432399ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe55ffbb04a61df1adde",
+    "url": "./static/js/main.9b97a444.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
